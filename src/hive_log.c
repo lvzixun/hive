711ac4f784ba8426cfbd72f51264e97ac34d0d4c
zixun <lvzixun@gmail.com>
2018-03-04 21:32:56 +0800

    remove max buffer 256

--- src/hive_log.c
+++ src/hive_log.c
@@ -3,28 +3,38 @@
 #include <stdlib.h>
 
 
-#define format2buffer(f) va_list args; \
-    va_start(args, f); \
-    char buffer[256] = {0}; \
-    vsnprintf(buffer, sizeof(buffer)-1, f, args); 
+// #define format2buffer(f) va_list args; \
+//     va_start(args, f); \
+//     char buffer[256] = {0}; \
+//     vsnprintf(buffer, sizeof(buffer)-1, f, args); 
 
 void
 hive_elog(const char* tag, const char* f, ...) {
-    format2buffer(f);
     tag = (tag == NULL)?(""):(tag);
-    fprintf(stderr, "![%s]: %s\n", tag, buffer);
+    fprintf(stderr, "![%s]: ", tag);
+    va_list args;
+    va_start(args, f);
+    vfprintf(stderr, f, args);
+    fprintf(stderr, "\n");
+    fflush(stderr);
 }
 
 void
 hive_printf(const char* f, ...) {
-    format2buffer(f);
-    fprintf(stdout, "%s\n", buffer);
+    va_list args;
+    va_start(args, f);
+    vfprintf(stdout, f, args);
+    fprintf(stdout, "\n");
+    fflush(stdout);
 }
 
 
 void
 hive_panic(const char* f, ...) {
-    format2buffer(f);
-    fprintf(stderr, "[hive panic] %s\n", buffer);
+    va_list args;
+    va_start(args, f);
+    vfprintf(stderr, f, args);
+    fprintf(stderr, "\n");
+    fflush(stderr);
     exit(1);
 }
