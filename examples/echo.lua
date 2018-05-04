e3107074339e155f0b8a7bda81d21dac6e71cf77
zixun <lvzixun@gmail.com>
2018-05-04 11:23:40 +0800

    update echo

--- examples/echo.lua
+++ examples/echo.lua
@@ -1,15 +1,11 @@
 local hive = require "hive"
 local socket = require "hive.socket"
 local thread = require "hive.thread"
+local logf = require("hive.log").logf
 
 local M = {}
 local Socket_M = {}
 
-
-local function printf(f, ...)
-    local s = string.format(f, ...)
-    print(s)
-end
 
 
 local source = [[
@@ -27,13 +23,13 @@ function M:echo(client_id)
     socket.attach(client_id)
     local s, err = socket.read(client_id)
     if s and #s>0 then
-        printf("\nrecv:%s from socket id:%s by actor:%s\n", s, client_id, SELF_HANDLE)
+        logf("\nrecv:%s from socket id:%s by actor:%s\n", s, client_id, SELF_HANDLE)
         socket.send(client_id, source)
         socket.close(client_id)
     elseif s and #s == 0 then
-        printf("socket id:%s is break", client_id)
+        logf("socket id:%s is break", client_id)
     else
-        printf("s:%s error:%s", s, err)
+        logf("s:%s error:%s", s, err)
     end
 end
 
